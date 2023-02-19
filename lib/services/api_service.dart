639705ca8a9eab3228c835afeb99d842064d6694
codinginpain <codinginpain@gmail.com>
2023-02-19 18:43:49 +0900

    detail screen, Future List

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
 
+import 'package:api_practice/models/webtoon_detail_model.dart';
+import 'package:api_practice/models/webtoon_episode_model.dart';
 import 'package:api_practice/models/webtoon_model.dart';
 import 'package:http/http.dart' as http;
 
@@ -19,6 +21,31 @@ class ApiService {
         webtoonInstances.add(instance);
       }
       return webtoonInstances;
+    }
+    throw Error();
+  }
+
+  static Future<WebToonDetailModel> getToonById(String id) async {
+    final url = Uri.parse("$baseUrl/$id");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final webtoon = jsonDecode(response.body);
+      return WebToonDetailModel.fromJson(webtoon);
+    }
+    throw Error();
+  }
+
+  static Future<List<WebtoonEpisodeModel>> getLatestEpisodeById(
+      String id) async {
+    List<WebtoonEpisodeModel> episodesInstances = [];
+    final url = Uri.parse("$baseUrl/$id/episodes");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final episodes = jsonDecode(response.body);
+      for (var episode in episodes) {
+        episodesInstances.add(WebtoonEpisodeModel.fromJson(episode));
+      }
+      return episodesInstances;
     }
     throw Error();
   }
